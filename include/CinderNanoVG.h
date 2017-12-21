886be39cf0d9f45d5363e3a060248030bd324f25
origamidance <origamidance@gmail.com>
2017-12-21 18:24:14 +0100

    cleaning

---
+++ include/CinderNanoVG.h
@@ -0,0 +1,9 @@
+//
+// Created by origamidance on 12/21/17.
+//
+
+#ifndef CINDERELLAAPP_CINDERNANOVG_H
+#define CINDERELLAAPP_CINDERNANOVG_H
+
+#endif //CINDERELLAAPP_CINDERNANOVG_H
+#include "ci_nanovg_gl.hpp"
